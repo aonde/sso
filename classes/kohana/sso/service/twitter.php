9fc300aa076f0e37a2f9129354a42ff9e7b3327b
Edgard Nogueira <edgard.nogueira@gmail.com>
2012-05-14 15:26:04 -0300

    faltou uma class do twitter

---
+++ classes/kohana/sso/service/twitter.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Kohana_SSO_Service_Twitter extends SSO_OAuth {
+
+	/**
+	 * @var  string  sso service name
+	 */
+	protected $sso_service = 'twitter';
+	
+}
