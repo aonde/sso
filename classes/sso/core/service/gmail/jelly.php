6c2c0041423d26b3ff72fcc8b83c78b60ee925be
Edgard Nogueira <edgard.nogueira@gmail.com>
2012-06-12 17:14:51 -0300

    adicionando google oauth2

---
+++ classes/sso/core/service/gmail/jelly.php
@@ -0,0 +1,45 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class SSO_Core_Service_Gmail_Jelly extends SSO_Service_Gmail {
+
+	/**
+	 * Completes the login and signs up a user if necessary.
+	 *
+	 * @return  boolean
+	 * @uses    Kohana::$log
+	 * @uses    Log::ERROR
+	 * @uses    Kohana_Exception::text
+	 * @uses    Jelly::factory
+	 * @uses    Auth::instance
+	 */
+	protected function complete_login()
+	{
+		try
+		{
+			// Get user details
+			$data = $this->fb->api('/me');
+		}
+		catch (gmailApiException $e)
+		{
+			// Log the error and return FALSE
+			Kohana::$log->add(Log::ERROR, Kohana_Exception::text($e));
+		    return FALSE;
+		}
+
+		// Set provider field
+		$provider_field = $this->sso_service.'_id';
+
+		// Check whether that id exists in our users table (provider id field)
+		$user = Jelly::factory('user')->find_sso_user($provider_field, $data);
+
+		// Signup if necessary
+		$signup = Jelly::factory('user')->sso_signup($user, $data, $provider_field);
+
+		// Give the user a normal login session
+		Auth::instance()->force_sso_login($signup);
+
+		// Login complete
+		return TRUE;
+	}
+
+} // End SSO_Core_Service_gmail_Jelly
---
+++ classes/sso/service/gmail/jelly.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class SSO_Service_Gmail_Jelly extends SSO_Core_Service_Gmail_Jelly {} // End SSO_Service_Facebook_Jelly
